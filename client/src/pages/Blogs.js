00eb06edd59ae552fe630d8ca890dbb99211b123
Ayaan Shaikh <69466895+AyaanShaikh28@users.noreply.github.com>
2023-12-31 16:42:37 +0530

    Update Blogs.js

--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -3,16 +3,16 @@ import axios from "axios";
 import BlogCard from "../components/BlogCard";
 
 const Blogs = () => {
-  // State ka initial setup karna, jisme blogs store hote hain
+  
   const [blogs, setBlogs] = useState([]);
 
-  // Sabhi blogs ko fetch karne ka function
+  
   const getAllBlogs = async () => {
     try {
-      // Axios ka upayog karke server se sabhi blogs retrieve karna
+      
       const { data } = await axios.get("/api/v1/blog/all-blog");
 
-      // Agar server dwara sabhi blogs retrieve successful hote hain, toh state ko update karna
+      
       if (data?.success) {
         setBlogs(data?.blogs);
       }
@@ -21,19 +21,18 @@ const Blogs = () => {
     }
   };
 
-  // useEffect hook ka upayog karke component mount hone par sabhi blogs ko fetch karna
+  
   useEffect(() => {
     getAllBlogs();
   }, []);
 
   return (
     <div>
-      {/* Agar blogs hain toh unko render karna */}
+      
       {blogs &&
         blogs.map((blog) => (
           <BlogCard
             id={blog?._id}
-            // Yadi user login hai aur blog usi user ka hai toh isUser prop ko true set karna, nahi toh false
             isUser={localStorage.getItem("userId") === blog?.user?._id}
             title={blog?.title}
             description={blog?.description}
