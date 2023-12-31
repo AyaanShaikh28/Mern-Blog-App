d29d7eac169724be3e468014c9d1b1d23472a320
Ayaan Shaikh <69466895+AyaanShaikh28@users.noreply.github.com>
2023-12-31 16:34:11 +0530

    Update UserBlogs.js

--- client/src/pages/UserBlogs.js
+++ client/src/pages/UserBlogs.js
@@ -3,19 +3,19 @@ import axios from "axios";
 import BlogCard from "../components/BlogCard";
 
 const UserBlogs = () => {
-  // State ka initial setup karna, jisme user ke blogs store hote hain
+  
   const [blogs, setBlogs] = useState([]);
 
-  // User ke blogs ko fetch karne ka function
+  
   const getUserBlogs = async () => {
     try {
-      // LocalStorage se user ID retrieve karna
+      
       const id = localStorage.getItem("userId");
 
-      // Axios ka upayog karke server se user ke blogs retrieve karna
+      
       const { data } = await axios.get(`/api/v1/blog/user-blog/${id}`);
 
-      // Agar server dwara user ke blogs retrieve successful hote hain, toh state ko update karna
+      
       if (data?.success) {
         setBlogs(data?.userBlog.blogs);
       }
@@ -24,17 +24,17 @@ const UserBlogs = () => {
     }
   };
 
-  // useEffect hook ka upayog karke component mount hone par user ke blogs ko fetch karna
+  
   useEffect(() => {
     getUserBlogs();
   }, []);
 
-  // Console par user ke blogs ko log karna (development ke liye)
+ 
   console.log(blogs);
 
   return (
     <div>
-      {/* Agar user ke blogs hain toh unko render karna */}
+      
       {blogs && blogs.length > 0 ? (
         blogs.map((blog) => (
           <BlogCard
@@ -48,7 +48,7 @@ const UserBlogs = () => {
           />
         ))
       ) : (
-        // Agar user ke paas koi blog nahi hai toh appropriate message dikhana
+        
         <h1>You Haven't Created a Blog</h1>
       )}
     </div>
